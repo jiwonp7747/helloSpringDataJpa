c7bfe7439043d6b606e2b14281cd2ea64d43244e
jiwon <angry9908@gmail.com>
2025-06-05 05:39:49 +0900

    feat: basic query

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repo/RoleRepository.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repo/RoleRepository.java
@@ -3,5 +3,8 @@ package kr.ac.hansung.cse.hellospringdatajpa.repo;
 import kr.ac.hansung.cse.hellospringdatajpa.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByRolename(String roleName);
 }
