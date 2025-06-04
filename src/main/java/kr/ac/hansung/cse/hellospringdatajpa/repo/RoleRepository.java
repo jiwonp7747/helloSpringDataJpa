5b7995f75678c7b22bba5dd2e736804d8750c0a9
jiwon <angry9908@gmail.com>
2025-06-05 05:17:24 +0900

    feat: user, role entity dao

---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repo/RoleRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.hansung.cse.hellospringdatajpa.repo;
+
+import kr.ac.hansung.cse.hellospringdatajpa.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repo/UserRepository.java
@@ -0,0 +1,7 @@
+package kr.ac.hansung.cse.hellospringdatajpa.repo;
+
+import kr.ac.hansung.cse.hellospringdatajpa.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
