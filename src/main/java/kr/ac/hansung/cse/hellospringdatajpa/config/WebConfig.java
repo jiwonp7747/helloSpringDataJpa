f6c408c53c36df96cf7b82f5f889993230fbb14e
jiwon <angry9908@gmail.com>
2025-06-05 05:39:19 +0900

    setting: config spring security

---
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/config/WebConfig.java
@@ -0,0 +1,21 @@
+package kr.ac.hansung.cse.hellospringdatajpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
+}
